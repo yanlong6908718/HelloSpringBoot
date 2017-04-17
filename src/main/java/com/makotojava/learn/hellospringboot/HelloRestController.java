27b05fdacdfcbb91f838f8dab79ee6955dcdca94
J Steven Perry <jstevenperry@gmail.com>
2017-04-17 10:50:32 -0500

    Initial commit

---
+++ src/main/java/com/makotojava/learn/hellospringboot/HelloRestController.java
@@ -0,0 +1,13 @@
+package com.makotojava.learn.hellospringboot;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloRestController {
+
+  @RequestMapping("/hello")
+  public String hello() {
+    return "Hello. All your base are belong to us.";
+  }
+}
